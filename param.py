4c1c740d0ca311e878d2d180da13af0c03582393
Andrew Jisan Yoon <68487218+andrew-jisan-yoon@users.noreply.github.com>
2020-11-18 22:28:23 +0900

    storing puzzles in a separate folder

--- param.py
+++ param.py
@@ -2,6 +2,7 @@
 font_name = "comicsans"
 font_size = 30
 board_size = (600, 600)
+board_color = (255, 255, 255)  # white in RGB sequence
 
 # Params used in definition of Sudoku
 sudoku_dimensions = (9, 9)
--- process/board.py
+++ process/board.py
@@ -1,34 +1,23 @@
-board = [
-    [5, 3, 0, 0, 7, 0, 0, 0, 0]
-    [6, 0, 0, 1, 9, 5, 0, 0, 0]
-    [0, 9, 8, 0, 0, 0, 0, 6, 0]
-    [8, 0, 0, 0, 6, 0, 0, 0, 3]
-    [4, 0, 0, 8, 0, 3, 0, 0, 1]
-    [7, 0, 0, 0, 2, 0, 0, 0, 6]
-    [0, 6, 0, 0, 0, 0, 2, 8, 0]
-    [0, 0, 0, 4, 1, 9, 0, 0, 5]
-    [0, 0, 0, 0, 8, 0, 0, 7, 9]
-]
+class Board:
 
-
-class Grid:
-
-    def __init__(self, rows, cols, width, height, board, surface):
-        self.cubes = [[Cube(board[i][j], i, j, width, height)
-                      for j in range(cols) for i in range(rows)]]
-        self.surface = surface
-        self.selected = None
-
-
-class Cube:
-    def __init__(self, value, row, col, width, height, font):
-        self.value = value
-        self.selected = False
-        self.length = width / 9
-        self.font = font
+    def __init__(self,
+                 window: "pygame.surface",
+                 puzzle: "list of lists"):
+        self.window = window
+        self.puzzle = puzzle
+        self.squares = [[Square(self.puzzle[i][j], (i, j))
+                        for j in len(self.puzzle)]
+                        for i in len(self.puzzle[0])]
 
     def draw():
         pass
 
-    def draw_change():
+
+class Square:
+    def __init__(self, value, coord):
+        self.value = value
+        self.coord = coord  # Coordinates by indices
+        self.is_selected = False
+
+    def draw():
         pass
