b7b75f990a149dc5022966865b854e52ca070138
Andrew Jisan Yoon <68487218+andrew-jisan-yoon@users.noreply.github.com>
2020-11-19 23:52:38 +0900

    draft of board drawing in main.py

--- main.py
+++ main.py
@@ -2,22 +2,28 @@ import pygame
 from param import *
 from src.board import *
 from src.process import *
+import json
 
-pygame.font.init()
+pygame.init()
 
 
 def main():
     display = pygame.display.set_mode(display_size)
     display.fill(display_color)
-    fnt = pygame.font.SysFont(font_name, font_size)
+    text_font = pygame.font.SysFont(font_name, font_size)
     pygame.display.set_caption("Sudoku")
-    status = {"is_running": True, "key": None}
+    puzzle = json.load(open("puzzle/00.json", "r"))
 
-    while status['is_running']:
-        for event in pygame.event.get():
-            status = event_response(event, status)
-
-        pygame.display.update()
+    board = Board(display, puzzle, text_font,
+                  line_color, selection_color, text_color)
+    board.draw()
+    pygame.display.update()
+    # status = {"is_running": True, "key": None}
+    # while status['is_running']:
+    #     for event in pygame.event.get():
+    #         status = event_response(event, status)
+    #
+    #     pygame.display.update()
 
 
 if __name__ == "__main__":
