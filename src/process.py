0a6c88f7be81ebe080422d75b6f225f1142d762a
Andrew Jisan Yoon <68487218+andrew-jisan-yoon@users.noreply.github.com>
2020-11-22 22:15:21 +0900

    deleted valid() function

--- src/process.py
+++ src/process.py
@@ -2,45 +2,6 @@ import pygame
 import sys
 
 
-def valid(board, value, location):
-    """
-    Checks if:
-        1) the given number overlaps with another in the same row or column
-        2) the given number overlaps with another in 3x3 sub-grid
-    Parameters:
-        location: (x_coord, y_coord)
-    """
-    idx_col = location[0]
-    idx_row = location[1]
-
-    # checks if overlap in the row
-    for col in range(sudoku_columns):
-        if idx_col == col:
-            continue
-        if board[idx_row][col] == value:
-            return False
-
-    # checks if overlap in the column
-    for row in range(sudoku_rows):
-        if idx_row == row:
-            continue
-        if board[row][idx_col] == value:
-            return False
-
-    grid_col = idx_col // 3 * 3
-    grid_row = idx_row // 3 * 3
-
-    # checks if overlap in 3x3 subgrid
-    for row in range(grid_row, grid_row + 3):
-        for col in range(grid_col, grid_col + 3):
-            if (row, col) == location:
-                continue
-            if board[row][col] == value:
-                return False
-
-    return True
-
-
 def event_response(board, io_status, event):
     if event.type == pygame.QUIT:
         pygame.quit()
