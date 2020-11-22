c9d56301e916faab45e6082de616450bec77c791
Andrew Jisan Yoon <68487218+andrew-jisan-yoon@users.noreply.github.com>
2020-11-22 23:39:58 +0900

    test_board.py file created

--- test/test.py
+++
@@ -1,8 +0,0 @@
-"""
-Design:
-
-1. Add a solution button at the bottom of the window.
-When the player clicks the button, a warning message
-comes up asking: "Would you want to see the solution and forfeit the game?"
-If the player answers yes, auto-complete the puzzle.
-"""
---
+++ test/test_board.py
@@ -0,0 +1,17 @@
+import pytest
+from src.board import *
+from param import *
+
+
+@pytest.fixture
+def board():
+    """Returns a Board instance"""
+    pass
+
+
+def test_setting_class_attr(board):
+    pass
+
+
+def test_draw(board):
+    pass
