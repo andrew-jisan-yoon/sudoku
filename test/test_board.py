9b618fb7720aa82829eee37db9ce0c73b612f1fe
Andrew Jisan Yoon <68487218+andrew-jisan-yoon@users.noreply.github.com>
2020-11-23 22:37:27 +0900

    pytest.fixture board() is drafted

--- test/test_board.py
+++ test/test_board.py
@@ -6,7 +6,11 @@ from param import *
 @pytest.fixture
 def board():
     """Returns a Board instance"""
-    pass
+    display = pygame.display.set_mode(display_size)
+    board = Board(display, init_text_font, input_text_font,
+                  display_color, line_color, selection_color,
+                  init_text_color, input_text_color)
+    return board
 
 
 def test_setting_class_attr(board):
