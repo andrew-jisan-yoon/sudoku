caf753aba9f261f596615bfd6bce257ef772262b
Andrew Jisan Yoon <68487218+andrew-jisan-yoon@users.noreply.github.com>
2020-11-26 16:49:17 +0900

    adding fonts to board fixture

--- test/test_board.py
+++ test/test_board.py
@@ -6,7 +6,10 @@ from param import *
 @pytest.fixture
 def board():
     """Returns a Board instance"""
+    pygame.init()
     display = pygame.display.set_mode(display_size)
+    init_text_font = pygame.font.SysFont(font_name, font_size, bold=True)
+    input_text_font = pygame.font.SysFont(font_name, font_size, italic=True)
     board = Board(display, init_text_font, input_text_font,
                   display_color, line_color, selection_color,
                   init_text_color, input_text_color)
