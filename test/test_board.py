891962f3d5b62568fda967cf6d9ca1aa75206784
Andrew Jisan Yoon <68487218+andrew-jisan-yoon@users.noreply.github.com>
2020-11-28 16:30:54 +0900

    removed board fixture and edited test functions

--- test/test_board.py
+++ test/test_board.py
@@ -5,37 +5,31 @@ from param import *
 pygame.init()
 
 
-@pytest.fixture
-def board():
-    """Returns a Board instance"""
-    display = pygame.display.set_mode(display_size)
-    init_text_font = pygame.font.SysFont(font_name, font_size, bold=True)
-    input_text_font = pygame.font.SysFont(font_name, font_size, italic=True)
-    board = Board(display, init_text_font, input_text_font,
-                  display_color, line_color, selection_color,
-                  init_text_color, input_text_color)
-    return board
-
-
-@pytest.mark.parametrize("mouse_pos,expected", [
-    ((display_size[0] // 2, display_size[1] // 2), (5, 5)),
-    ((display_size[0] * 2, display_size[1] * 2), None)
+@pytest.mark.parametrize("puzzle, mouse_pos, expected", [
+    ([[0]], (display_size[0] // 2, display_size[1] // 2), (1, 1)),
+    ([[0]], (display_size[0] * 2, display_size[1] * 2), None)
 ])
-def test_select_square_with_zero_initial_value(board, mouse_pos, expected):
-    result = board.select_square(mouse_pos)
+def test_select_square_with_zero_initial_value(puzzle, mouse_pos, expected):
+    result = Board(puzzle).select_square(mouse_pos)
     assert result == expected
 
 
-@pytest.mark.parametrize("mouse_pos,expected", [])
-def test_select_square_with_nonzero_initial_value(board, mouse_pos, expected):
+@pytest.mark.parametrize("puzzle, mouse_pos, expected", [
+    ([[9]], (display_size[0] // 2, display_size[1] // 2), None),
+    ([[9]], (display_size[0] * 2, display_size[1] * 2), None)
+])
+def test_select_square_with_nonzero_initial_value(puzzle, mouse_pos, expected):
     """
     select_square() should return None for squares with nonzero initial values
     """
     pass
 
 
-# @pytest.mark.parametrize("coord,value,expected", [
-#     ((5, 5), ,)
-# ])
-def test_place_value(board):
-    pass
+@pytest.mark.parametrize("puzzle, coord, value, expected", [
+     ([[0]], (1, 1), 1, 1),
+     ([[1]], (1, 1), 4, None)
+])
+def test_place_value(puzzle, coord, value, expected):
+    board = Board(puzzle)
+    board.place_value(coord, value)
+    assert board.puzzle[coord[1]][coord[0]].input_value == expected
