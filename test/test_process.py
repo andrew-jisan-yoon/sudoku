b6d63759183912ce9525fc3b0675baae2e7725e3
Andrew Jisan Yoon <68487218+andrew-jisan-yoon@users.noreply.github.com>
2020-12-03 21:29:06 +0900

    filled in test_autocomplete()

--- test/test_process.py
+++ test/test_process.py
@@ -10,5 +10,17 @@ def test_event_response():
     pass
 
 
-def test_autocomplete():
+@pytest.mark.parametrize("puzzle_json, expected", [
+    ([[1, 0, 0], [2, 0, 7], [0, 6, 0]], [[1, 5, 3], [2, 8, 7], [4, 6, 9]])
+])
+def test_autocomplete(puzzle_json, expected):
+    puzzle = Puzzle(puzzle_json)
+    autocomplete(puzzle)
+    result = [[puzzle.squares[y][x].input_entered
+              for x in range(len(puzzle.squares[0]))]
+              for y in range(len(puzzle.squares))]
+    assert result == expected
+
+
+def test_find_empty():
     pass
